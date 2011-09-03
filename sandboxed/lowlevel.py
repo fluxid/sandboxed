68be8ed9f0a3cce7c94c3d691abedc0520d68f2d
Tomasz Fluxid Kowalczyk <accreg-git@fluxid.pl>
2011-09-03 20:49:39 +0200

    Failed attempt to use glibc clone() call instead of using syscall directly... Let it stay commented.

--- sandboxed/lowlevel.py
+++ sandboxed/lowlevel.py
@@ -27,15 +27,28 @@ getpid = syscall(const.SYS_getpid)
 getppid = syscall(const.SYS_getppid)
 pivot_root = ccall('pivot_root', True, ct.c_int, ct.c_char_p, ct.c_char_p)
 _clone = syscall(const.SYS_clone, ct.c_int, ct.c_void_p)
+#_clone = ccall('clone', True, ct.c_int, ct.c_void_p, ct.c_void_p, ct.c_int, ct.c_void_p)
 _mount = ccall('mount', True, ct.c_int, ct.c_char_p, ct.c_char_p, ct.c_char_p, ct.c_ulong, ct.c_void_p)
 umount = ccall('umount', True, ct.c_int, ct.c_char_p)
 umount2 = ccall('umount2', True, ct.c_int, ct.c_char_p, ct.c_int)
 _sethostname = ccall('sethostname', True, ct.c_int, ct.c_char_p, ct.c_int)
 _gethostname = ccall('gethostname', True, ct.c_int, ct.c_char_p, ct.c_int)
 
+#_CLONE_CALLBACK = ct.CFUNCTYPE(ct.c_int, ct.c_void_p)
+#_CLONE_STACK_SIZE = 65535
+#_CloneStack = ct.c_char * _CLONE_STACK_SIZE
+
 # Convenience wrappers
 def clone(flags):
     return _clone(flags, None)
+
+#def clone(callback, flags):
+#    callback = lambda x: callback() or 0
+#    callback_p = _CLONE_CALLBACK(callback)
+#    child_stack = _CloneStack()
+#    child_stack_p = ct.pointer(ct.pointer(child_stack)[_CLONE_STACK_SIZE-1])
+#    pid = _clone(callback_p, child_stack_p, flags, None)
+#    os.waitpid(pid, 0)
 
 def sethostname(hostname):
     _sethostname(hostname.encode(), len(hostname))
