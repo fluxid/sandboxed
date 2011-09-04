3f8e782b48f76be411326f8aeec26210c4d1bd33
Tomasz Fluxid Kowalczyk <accreg-git@fluxid.pl>
2011-09-04 18:44:48 +0200

    Be serious about limiting time

--- remote_exec.py
+++ remote_exec.py
@@ -57,26 +57,26 @@ class ExecJail(Jail):
             if pids:
                 while True:
                     try:
-                        pid, _ = os.waitpid(0, os.WNOHANG)
+                        pid, status = os.waitpid(0, os.WNOHANG)
                     except OSError as exc:
                         if exc.errno == errno.ECHILD:
                             break
                         raise
                     if pid in pids:
-                        print('removing pid {}'.format(pid))
                         pids.remove(pid)
+                        print('removing pid: {} status: {} workers: {}'.format(pid, status, len(pids)))
 
         def handle_signal(signum, frame):
             nonlocal running, pids
             running = False
             for pid in pids:
-                print('SIGTERM pid {}'.format(pid))
+                print('SIGTERM pid: {}'.format(pid))
                 os.kill(pid, signal.SIGTERM)
             time.sleep(0.2)
             wait_for_pids()
             if pids:
                 for pid in pids:
-                    print('SIGKILL pid {}'.format(pid))
+                    print('SIGKILL pid: {}'.format(pid))
                     os.kill(pid, signal.SIGKILL)
                 time.sleep(0.2)
                 wait_for_pids()
@@ -93,8 +93,8 @@ class ExecJail(Jail):
                         conn, _ = sock.accept()
                         pid = os.fork()
                         if pid:
-                            print('adding pid {}'.format(pid))
                             pids.add(pid)
+                            print('adding pid: {} workers: {}'.format(pid, len(pids)))
                         else:
                             self.process_connection(conn)
                     else:
@@ -107,19 +107,28 @@ class ExecJail(Jail):
             sock.close()
 
     def process_connection(self, connection):
+        my_stdout = io.StringIO()
+
+        def exit(status):
+            connection.sendall(my_stdout.getvalue().encode('utf-8'))
+            connection.send(b'\0')
+            connection.close()
+            os._exit(status)
+
         try:
             resource.setrlimit(resource.RLIMIT_NPROC, (1,1))
             mem_bytes = 1024*1024*5
             resource.setrlimit(resource.RLIMIT_AS, (mem_bytes, mem_bytes))
-            resource.setrlimit(resource.RLIMIT_CPU, (3, 7))
-
+            resource.setrlimit(resource.RLIMIT_CPU, (3, 5))
+            
             def handle_signal(signum, frame):
                 if signum == signal.SIGALRM:
-                    raise Exception('Six seconds of execution passed, giving up')
+                    print('Six seconds of execution passed, giving up')
                 elif signum == signal.SIGXCPU:
-                    raise Exception('Three seconds of CPU time passed, giving up')
+                    print('Three seconds of CPU time passed, giving up')
                 else:
-                    os._exit(1)
+                    print('Signal received')
+                exit(1)
 
             signal.signal(signal.SIGALRM, handle_signal)
             signal.signal(signal.SIGXCPU, handle_signal)
@@ -138,26 +147,22 @@ class ExecJail(Jail):
                     continue_reading = False
                 in_data.write(data)
 
-            old_stdout, old_stderr = sys.stdout, sys.stderr
-            sys.stdout = sys.stderr = io.StringIO()
+            sys.stdout = sys.stderr = my_stdout
 
             signal.alarm(6)
             try:
                 globals_ = dict()
                 exec(in_data.getvalue(), globals_)
             except:
+                del globals_
                 exception = traceback.print_exc()
             finally:
                 signal.alarm(0)
-            
-            connection.send(sys.stdout.getvalue().encode('utf-8') + b'\0')
         finally:
-            sys.stdout = sys.stderr = old_stdout, old_stderr
-            connection.close()
-            os._exit(0)
+            exit(0)
 
 def main():
-    ExecJail('socket', 2000, 'fluxid', 'fluxid', 'lolnope', remount_ro=False, mount_dev=True).run()
+    ExecJail('socket', 2000, 'fluxid', 'fluxid', 'lolnope', remount_ro=True, mount_dev=True).run()
 
 if __name__ == '__main__':
     main()
